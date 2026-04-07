08221fc44d43e746afea1ba4046ada9682b0f2b2
Xahim <111353330+XahimA@users.noreply.github.com>
2026-04-07 17:21:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(__dirname));
+
+// Explicit route for root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
